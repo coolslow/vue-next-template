daf44559962de1321ac56f05a4aa41528b620817
mizhon <mizhon@live.cn>
2020-10-04 10:44:38 +0800

    添加.babelrc配置及相关依赖库，以允许使用@Prop、@Watch

--- src/shims-vue.d.ts
+++ src/shims-vue.d.ts
@@ -3,3 +3,9 @@ declare module '*.vue' {
   const component: ReturnType<typeof defineComponent>
   export default component
 }
+
+// declare module '*.vue' {
+//   import { Component } from 'vue'
+//   var component: Component
+//   export default component
+// }
