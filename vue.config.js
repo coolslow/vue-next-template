d205cd0a8d2facaab474269b5d79edfc77798359
mizhon <mizhon@live.cn>
2020-10-05 00:02:42 +0800

    添加iconfont相关资源

--- vue.config.js
+++ vue.config.js
@@ -22,12 +22,13 @@ module.exports = {
       extensions: ['ts', 'tsx', '.vue', '.js', '.json'],
       alias: {
         '@': resolve('src'),
-        '@apis': resolve('src/apis'),
-        '@assets': resolve('src/assets'),
-        '@requests': resolve('src/common/requests'),
-        '@utils': resolve('src/common/utils'),
-        '@components': resolve('src/components'),
-        '@views': resolve('src/views')
+        '@apis': resolve('src/apis'), // 接口相关
+        '@assets': resolve('src/assets'), // 静态资源相关
+        '@auth': resolve('src/common/auth'), // 鉴权相关
+        '@utils': resolve('src/common/utils'), // 工具包
+        '@requests': resolve('src/common/requests'), // 请求相关
+        '@components': resolve('src/components'), // 组件部分
+        '@views': resolve('src/views') // 业务相关页面
       }
     }
   }
